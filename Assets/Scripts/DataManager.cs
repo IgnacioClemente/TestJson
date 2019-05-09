06e6daeef42cabee0286f7b29787ae64ae30a8f4
hansow <nachoagus@live.com.ar>
2019-05-09 00:14:38 -0300

    Initial Commit

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataManager : MonoBehaviour
+{
+    public WeaponDataCollection WeaponDataCollection;
+
+    string dataToSave;
+
+    private void Awake()
+    {
+        WeaponData espada = new WeaponData(5, 4, "Espada");
+        WeaponData arco = new WeaponData(8, 1, "Arco");
+        WeaponData daga = new WeaponData(3, 5, "Daga");
+
+        WeaponDataCollection.WeaponData = new List<WeaponData>();
+        WeaponDataCollection.WeaponData.Add(espada);
+        WeaponDataCollection.WeaponData.Add(arco);
+        WeaponDataCollection.WeaponData.Add(daga);
+
+        dataToSave = JsonUtility.ToJson(WeaponDataCollection);
+        Debug.Log(dataToSave);
+        SaveData();
+    }
+
+    public void SaveData()
+    {
+        if(!File.Exists(Application.streamingAssetsPath + "weapons.json"))
+            File.WriteAllText(Application.streamingAssetsPath + "weapons.json", dataToSave);
+    }
+}
---
+++ Assets/Scripts/WeaponData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WeaponData
+{
+    public int Daño;
+    public int Defensa;
+    public string Nombre;
+
+    public WeaponData(int daño, int defensa, string nombre)
+    {
+        Daño = daño;
+        Defensa = defensa;
+        Nombre = nombre;
+    }
+}
